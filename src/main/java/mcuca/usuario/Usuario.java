daa08fb9724610b48247abf3f426e20b2a057edc
Antonio Ruiz Rondán <toninoes@gmail.com>
2017-05-08 18:58:08 +0200

    Modificada Usuario.java

--- src/main/java/mcuca/usuario/Usuario.java
+++ src/main/java/mcuca/usuario/Usuario.java
@@ -3,14 +3,22 @@ package mcuca.usuario;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.GenerationType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+import mcuca.establecimiento.Establecimiento;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+
 
 @Entity
 public class Usuario implements UserDetails {
@@ -18,8 +26,10 @@ public class Usuario implements UserDetails {
 	private static final long serialVersionUID = -7245278202840772378L;
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
+	
+	private String dni;
 
 	private String firstName;
 
@@ -28,29 +38,44 @@ public class Usuario implements UserDetails {
 	private String username;
 
 	private String password;
+	
+	private Rol rol;
+	
+	@ManyToMany(targetEntity=Establecimiento.class)
+	@JoinTable(name = "establecimiento_usuario", joinColumns = @JoinColumn(name = "usuario_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "establecimiento_id", referencedColumnName = "id"))
+	private Set<Establecimiento> establecimientos;
 
 	public Long getId() { return id; }
+	public String getDni() { return dni; }
 	public String getFirstName() { return firstName; }
 	public String getLastName() { return lastName; }
 	@Override
 	public String getPassword() { return this.password; }
 	@Override
 	public String getUsername() { return this.username; }
+	public Rol getRol() { return rol; }
+	public Set<Establecimiento> getEstablecimientos() { return establecimientos; }
 
+	public void setDni(String dni) { this.dni = dni; }
 	public void setFirstName(String firstName) { this.firstName = firstName; }
 	public void setLastName(String lastName) { this.lastName = lastName; }
 	public void setUsername(String username) { this.username= username; }
 	public void setPassword(String password) { this.password = password; }
+	public void setRol(Rol rol) { this.rol = rol; }
+	public void setEst(Set<Establecimiento> establecimientos) { this.establecimientos = establecimientos; }
 
 	public Usuario() {}
-	public Usuario(String firstName, String lastName, String username)
+	
+	public Usuario(String dni, String firstName, String lastName, String username)
 	{
+		this.dni = dni;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.username = username;
 	}
-	public Usuario(String firstName, String lastName) {
-		this(firstName,lastName,firstName);
+	
+	public Usuario(String dni, String firstName, String lastName) {
+		this(dni, firstName,lastName,firstName);
 	}
 
 	@Override
@@ -62,10 +87,13 @@ public class Usuario implements UserDetails {
 
 	@Override
 	public boolean isAccountNonExpired() { return true; }
+	
 	@Override
 	public boolean isAccountNonLocked() { return true; }
+	
 	@Override
 	public boolean isCredentialsNonExpired() { return true; }
+	
 	@Override
 	public boolean isEnabled() { return true; }
 
