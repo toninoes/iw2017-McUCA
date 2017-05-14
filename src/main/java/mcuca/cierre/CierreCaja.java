a775d6dd4a606cf26def464228358996987225b5
Manuel López <manuel.lopzjimz@gmail.com>
2017-05-14 17:03:04 +0200

    Include CierreCaja

---
+++ src/main/java/mcuca/cierre/CierreCaja.java
@@ -0,0 +1,37 @@
+package mcuca.cierre;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import mcuca.establecimiento.Establecimiento;
+
+@Entity
+public class CierreCaja {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	@ManyToOne
+	private Establecimiento establecimiento;
+	
+	private Date fechaCierre;
+	
+	private float recaudacion;
+	
+	public CierreCaja() {}
+	
+	public Long getId() { return this.id; }
+	public Establecimiento getEstablecimiento() { return this.establecimiento; }
+	public Date getFechaCierre() { return this.fechaCierre; }
+	public float getRecaudacion() { return this.recaudacion; }
+	
+	public void setEstablecimiento(Establecimiento establecimiento) { this.establecimiento = establecimiento; }
+	public void setFechaCierre(Date fecha) { this.fechaCierre = fecha; }
+	public void setRecaudacion(float recaudacion) { this.recaudacion = recaudacion; }
+
+}
