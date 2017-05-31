af19a9d95f27e043364ad1971144339186324b02
Antonio Ruiz Rondán <toninoes@gmail.com>
2017-05-31 19:31:01 +0200

    Modificado ProductoEditor.java. Permite subir imágenes del producto.

--- src/main/java/mcuca/producto/ProductoEditor.java
+++ src/main/java/mcuca/producto/ProductoEditor.java
@@ -15,6 +15,9 @@ import com.vaadin.ui.CssLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.TwinColSelect;
+import com.vaadin.ui.Upload;
+import com.vaadin.ui.Upload.Receiver;
+import com.vaadin.ui.Upload.SucceededListener;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.themes.ValoTheme;
@@ -22,18 +25,44 @@ import com.vaadin.ui.themes.ValoTheme;
 import mcuca.ingrediente.Ingrediente;
 import mcuca.ingrediente.IngredienteRepository;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
+import com.vaadin.data.Binder;
+import com.vaadin.event.ShortcutAction;
+import com.vaadin.server.FileResource;
+import com.vaadin.spring.annotation.SpringComponent;
+import com.vaadin.spring.annotation.UIScope;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.CssLayout;
+import com.vaadin.ui.Image;
+import com.vaadin.ui.JavaScript;
+import com.vaadin.ui.NativeSelect;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.Upload;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Upload.Receiver;
+import com.vaadin.ui.Upload.SucceededEvent;
+import com.vaadin.ui.Upload.SucceededListener;
+import com.vaadin.ui.themes.ValoTheme;
+
 @SpringComponent
 @UIScope
 @SuppressWarnings("serial")
 public class ProductoEditor extends VerticalLayout {
-
+	
 	private final ProductoRepository repoProducto;
 	private final IngredienteRepository repoIngrediente;
-
+	
 	private Set<Ingrediente> sSelected;
-
+	
 	private Producto producto;
-
+	
 	/* Fields to edit properties in Mesa entity */
 	Label title = new Label("Nuevo Producto");
 	TextField nombre = new TextField("Nombre");
@@ -41,8 +70,8 @@ public class ProductoEditor extends VerticalLayout {
 	TextField iva = new TextField("I.V.A.");
 	TextField foto = new TextField("Foto");
 	TwinColSelect<Ingrediente> ingredientes = new TwinColSelect<>("Ingrediente");
-
-
+	
+	
 	/* Action buttons */
 	Button guardar = new Button("Guardar");
 	Button cancelar = new Button("Cancelar");
@@ -50,13 +79,44 @@ public class ProductoEditor extends VerticalLayout {
 	CssLayout acciones = new CssLayout(guardar, cancelar, borrar);
 
 	Binder<Producto> binder = new Binder<>(Producto.class);
-
+	
 	@Autowired
 	public ProductoEditor(ProductoRepository repoProducto, IngredienteRepository repoIngrediente) {
 		this.repoProducto = repoProducto;
-		this.repoIngrediente = repoIngrediente;
+		this.repoIngrediente = repoIngrediente; 
 		ingredientes.setItems((Collection<Ingrediente>) repoIngrediente.findAll());
-		addComponents(title, nombre, precio, iva, foto, ingredientes, acciones);
+		
+		final Image imagen = new Image();
+		class ImageUploader implements Receiver, SucceededListener {
+			public File file;
+			public OutputStream receiveUpload(String filename, String mimeType){
+				FileOutputStream fos = null;
+				try{
+					file = new File("src/main/webapp/VAADIN/img/"+filename);
+					fos = new FileOutputStream(file);
+				}catch(final java.io.FileNotFoundException e){
+					JOptionPane.showMessageDialog(null, "Error al subir el fichero");
+					return null;
+				}
+				return fos;
+			}
+	
+			public void uploadSucceeded(SucceededEvent event) {
+				imagen.setVisible(true);
+				imagen.setSource(new FileResource(file));
+				imagen.setWidth(200, Unit.PIXELS);
+				imagen.setHeight(200, Unit.PIXELS);
+				foto.setValue(file.toString());
+			}
+		};
+		
+		ImageUploader receiver = new ImageUploader();
+		Upload upload = new Upload("Subir fichero", receiver);
+		upload.setImmediateMode(true);
+		upload.addSucceededListener(receiver);
+		foto.setVisible(false);
+				
+		addComponents(title, nombre, precio, iva, upload, imagen, ingredientes, acciones, foto);
 
 		// bind using naming convention
 		binder.forField(precio)
@@ -64,13 +124,13 @@ public class ProductoEditor extends VerticalLayout {
 		  .withConverter(
 		    new StringToDoubleConverter("Por favor introduce un número"))
 		  .bind("precio");
-
+		
 		binder.forField(iva)
 		  .withNullRepresentation("")
 		  .withConverter(
 		    new StringToDoubleConverter("Por favor introduce un número"))
 		  .bind("iva");
-
+		
 		binder.bindInstanceFields(this);
 
 		// Configure and style components
@@ -83,14 +143,14 @@ public class ProductoEditor extends VerticalLayout {
 		guardar.addClickListener(this::salvar);
 		borrar.addClickListener(e -> repoProducto.delete(producto));
 		cancelar.addClickListener(e -> editarProducto(producto));
-
+		
 		ingredientes.addSelectionListener(e -> {
 			sSelected = e.getAddedSelection();
 		});
-
+		
 		setVisible(false);
 	}
-
+	
 	public void salvar(ClickEvent event) {
 		binder.setBean(producto);
 		producto.setNombre(nombre.getValue());
